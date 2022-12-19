3c234fe4fd749498f34d4e6d6b7b36cfbca4b488
NimaQu <i@nimaqu.com>
2022-12-19 07:09:27 -0600

    添加项目文件。

---
+++ Data/fakaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using faka.Models;
+
+namespace faka.Data
+{
+    public class fakaContext : DbContext
+    {
+        public fakaContext (DbContextOptions<fakaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<faka.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace faka.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public bool IsEnabled { get; set; }
+    public bool IsHidden { get; set; }
+    public int Stock { get; set; }
+}
