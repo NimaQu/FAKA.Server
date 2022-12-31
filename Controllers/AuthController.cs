ac07d11d7f26bb26d5acac85a302d0d366131a4f
NimaQu <i@nimaqu.com>
2022-12-31 09:36:41 -0600

    增加迁移 api

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,7 +6,6 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using faka.Filters;
 using Microsoft.AspNetCore.Authorization;
 
 namespace faka.Controllers
---
+++ Controllers/MigrationController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using faka.Auth;
+using faka.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace faka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MigrationController : ControllerBase
+    {
+        private readonly fakaContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public MigrationController(fakaContext context, UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            // 依赖注入
+            _context = context;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        
+        [HttpGet(), Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Migration()
+        {
+            var pendingMigrations = await _context.Database.GetPendingMigrationsAsync();
+            if (pendingMigrations.Any())
+            {
+                return Ok(pendingMigrations);
+            }
+
+            return NotFound("No pending migrations");
+        }
+        
+        [HttpPost("run"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult> RunMigration()
+        {
+            await _context.Database.MigrateAsync();
+            return Ok("Yeh, You are now on .NET ASP Core");
+        }
+
+    }   
+}
